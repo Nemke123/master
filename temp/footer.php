9d078c77de71eedad3700cba269a22e359e64cf8
Nemke123 <47119467+Nemke123@users.noreply.github.com>
2019-08-15 09:49:02 +0200

    Add files via upload

---
+++ temp/footer.php
@@ -0,0 +1,8 @@
+<footer class="blog-footer">
+    <p>Copyright &copy; 2019</p>
+    <p>
+        <a href="/index.php">Home</a>
+        <a style="padding: 0 10px;" href="/login.php">Log In</a>
+        <a href="/create-post.php">Create a post</a>
+    </p>
+</footer>
